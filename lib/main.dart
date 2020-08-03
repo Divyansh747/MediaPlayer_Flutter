49b7af28c5f5e93555fc180b018d4296997e8ff1
divyansh747 <divyansh.1999.dr@gmail.com>
2020-08-04 00:53:41 +0530

    add tabbar view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'ui/LocalAudio.dart';
+import 'ui/LocalVideo.dart';
+import 'ui/NetworkAudio.dart';
+import 'ui/NetworkVideo.dart';
 
 main() {
   runApp(Media());
@@ -7,15 +11,52 @@ main() {
 class Media extends StatelessWidget {
   build(BuildContext b) {
     return MaterialApp(
-debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
           appBar: AppBar(
             title: Text("Media Player"),
             backgroundColor: Colors.blue,
-      ),
-     ),
-      body: Container(
-        //To be continued
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.audiotrack),
+                  text: "Audio",
+                ),
+                Tab(
+                  icon: Icon(Icons.voicemail),
+                  text: "Audio URL",
+                ),
+                Tab(
+                  icon: Icon(Icons.videocam),
+                  text: "Video",
+                ),
+                Tab(
+                  icon: Icon(Icons.music_video),
+                  text: "Video URL",
+                ),
+              ],
+            ),
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [.3, .9],
+                  colors: [Colors.blue, Colors.cyanAccent]),
+            ),
+            child: TabBarView(
+              children: [
+                LocalAudio(),
+                NetworkAudio(),
+                LocalVideo(),
+                NetworkVideo(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
